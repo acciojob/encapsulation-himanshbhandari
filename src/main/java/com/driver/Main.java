9ecce12c8fbd6753dd06bc9e3b01783f0a7ba7f6
Himanshu Bhandari <himansh8755529677@gmail.com>
2024-09-16 10:11:47 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String []args){
+        RWOnly ro=new RWOnly();
+//    rwOnly.name;  //this give error because cant access private member using dot operator
+
+        ro.setName("new name set");
+        ro.getName();
+        System.out.println(ro.getName());
+    }
 }
