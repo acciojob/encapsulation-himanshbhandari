4f07f14e48a5b55c6d9eb8ca8f03e38a75300a02
Himanshu Bhandari <himansh8755529677@gmail.com>
2024-09-16 10:16:16 +0530

    add comment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String []args){
         RWOnly ro=new RWOnly();
-//    rwOnly.name;  //this give error because cant access private member using dot operator
+//    ro.name;  //this give error because cant access private member using dot operator
 
         ro.setName("new name set");
         ro.getName();
